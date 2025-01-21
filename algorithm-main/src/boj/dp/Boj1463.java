c81927e1705825e181f2f5c694f8b9aa355922ab
ubiies <alal55al9@naver.com>
2025-01-21 14:04:14 +0900

    feat : 20250121

---
+++ algorithm-main/src/boj/dp/Boj1463.java
@@ -0,0 +1,28 @@
+package boj.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.util.Collections.min;
+
+/**
+ * 1로 만들기
+ */
+public class Boj1463 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+
+        for (int i=2;i<n+1;i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+        System.out.println(dp[n]);
+    }
+}
